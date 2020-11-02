68ca20dab342357b33436f98c712e0182bfe0112
alexandraalvarez <alexandra.alvarez.a@gmail.com>
2020-11-02 12:57:56 -0300

    iniciando v8, template App

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div>
+      <nav class="navbar navbar-light navbar-expand-lg fixed-top" id="nav8">
+        <div class="container">
+          <a class="navbar-brand">V8</a>
+          <button data-toggle="collapse" data-target="#navbarResponsive" class="navbar-toggler" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
+          <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="nav navbar-nav ml-auto">
+              <li class="nav-item" role="presentation"><router-link :to="{name: 'home'}">Portada</router-link></li>
+              <li class="nav-item" role="presentation"><router-link :to="{name: 'aboutme'}">Sobre mí</router-link></li>
+              <li class="nav-item" role="presentation"><router-link :to="{name: 'contact'}">Contacto</router-link></li>
+              <li class="nav-item" role="presentation"><router-link to="/post/lastpost">Último post</router-link></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    <router-view></router-view>
     </div>
-    <router-view/>
   </div>
 </template>
 
